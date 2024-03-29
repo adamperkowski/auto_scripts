94addc39d58eadb1e49d3dcd378ae5504ba81a15
Adam Perkowski <adas1per@protonmail.com>
2024-03-29 03:39:49 +0100

    for

--- autocpu.py
+++ autocpu.py
@@ -18,7 +18,8 @@ driver.implicitly_wait(10)
 blanklinks = driver.find_elements(by=By.XPATH, value="//div[@class='gray-all clearfix ags-list-box']")
 adlinks = driver.find_elements(by=By.XPATH, value="//div[@class='text-overflow ags-description']")
 
-for i in range(len(blanklinks), len(adlinks)):
+# for i in range(len(blanklinks), len(adlinks)):
+for i in range(len(adlinks)):
     adlinks[i].find_element(by=By.CSS_SELECTOR, value="span").click()
     sleep(int(adlinks[i].find_element(by=By.XPATH, value="../div[@class='ags-detail-point-time-report']").find_element(by=By.CLASS_NAME, value="ags-detail-time").text) + 15)
     tabs = driver.window_handles
@@ -26,5 +27,4 @@ for i in range(len(blanklinks), len(adlinks)):
     driver.close()
     driver.switch_to.window(tabs[0])
 
-# print(len(blanklinks))
-# print(adlinks)
+
