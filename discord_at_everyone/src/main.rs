f8a6eef8c0d057285afb45e628d5829599d0b137
Adam Perkowski <adas1per@protonmail.com>
2024-04-03 21:53:19 +0200

    argv

--- discord_at_everyone/src/main.rs
+++ discord_at_everyone/src/main.rs
@@ -1,7 +1,12 @@
 use std::env;
 
 fn main() {
-    if env::args().len() != 2 {
-        panic!("where args????");
-    }
+    println!("for grupz");
+    let argv: Vec<_> = env::args().collect();
+    let group_id: &str;
+
+    if argv.len() < 2 { panic!("where args????"); }
+    else { group_id = argv[1].as_str(); }
+
+    println!("https://discord.com/channels/@me/{}", group_id);
 }
