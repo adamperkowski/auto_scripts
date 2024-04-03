f58095603bc0fcbc9b9aebed076fc3c443a0ba1c
Adam Perkowski <adas1per@protonmail.com>
2024-04-03 22:05:39 +0200

    oki

--- discord_at_everyone/src/main.rs
+++ discord_at_everyone/src/main.rs
@@ -1,6 +1,9 @@
 use std::env;
 
-fn main() {
+use thirtyfour::prelude::*;
+
+#[tokio::main]
+async fn main() {
     println!("for grupz");
     let argv: Vec<_> = env::args().collect();
     let group_id: &str;
@@ -8,5 +11,10 @@ fn main() {
     if argv.len() < 2 { panic!("where args????"); }
     else { group_id = argv[1].as_str(); }
 
-    println!("https://discord.com/channels/@me/{}", group_id);
+    let chromium_conf_path = env::var("ACPU_CHROMIUM_CONF_PATH").unwrap();
+    let caps = DesiredCapabilities::chrome();
+    let driver = WebDriver::new("http://localhost:8515", caps).await?;
+
+    let url = format!("https://discord.com/channels/@me/{}", group_id);
+    driver.goto(url).await;
 }
