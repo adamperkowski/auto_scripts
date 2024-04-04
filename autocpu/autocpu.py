b57d74ea615f9ede97d981a0997e1850a1831cb5
Adam Perkowski <adas1per@protonmail.com>
2024-04-05 01:08:55 +0200

    slicz

--- autocpu/autocpu.py
+++ autocpu/autocpu.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 
 from time import sleep
 
-def harvest(url):
+def harvest(url, mode):
     driver.get(url)
 
     driver.implicitly_wait(10)
@@ -25,9 +25,12 @@ def harvest(url):
         adlinks[i].find_element(by=By.CSS_SELECTOR, value="span").click()
         print(f'Clicked {i} .')
         slp = int(adlinks[i].find_element(by=By.XPATH, value="../div[@class='ags-detail-point-time-report']").find_element(by=By.CLASS_NAME, value="ags-detail-time").text) + 25
+        tabs = driver.window_handles
+        if mode == 'surf':
+            print('Switching.')
+            driver.switch_to.window(tabs[0])
         print(f'Waiting {slp} .')
         sleep(slp)
-        tabs = driver.window_handles
         driver.switch_to.window(tabs[1])
         print(f'Closing {i} .')
         driver.close()
@@ -46,7 +49,7 @@ options.add_argument(f"--user-data-dir={getenv('ACPU_CHROMIUM_CONF_PATH')}")
 options.add_argument(r'--profile-directory=Profile 1')
 driver = webdriver.Chrome(options=options)
 
-harvest(getenv('ACPU_SURF_URL'))
-harvest(getenv('ACPU_ACTV_URL'))
+harvest(getenv('ACPU_SURF_URL'), 'surf')
+harvest(getenv('ACPU_ACTV_URL'), 'actv')
 
 print('Finished .')
